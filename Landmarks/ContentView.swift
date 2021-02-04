47518f1ed975f9ed2f246095526d64278656d2bf
Ehco1996 <zh19960202@gmail.com>
2021-02-04 08:24:48 +0800

    day1: landmarks

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("景明佳园")
+                    .font(.title)
+
+                HStack {
+                    Text("雨花台区 铁心桥街道")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("南京市")
+                        .font(.subheadline)
+
+                }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+
+
+                Divider()
+                Text("关于这里")
+                    .font(.title2)
+                Text("是我家")
+
+            }
+                .padding()
+            Spacer()
+        }
     }
 }
 
---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,10 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by ehco on 2021/2/4.
+//
+
+import Foundation
+
+
